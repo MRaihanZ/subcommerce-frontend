675b6603efd93bfb117abf0ada91a44ffa652b6b
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-06-24 18:47:09 +0700

    feat: adding experiment with buttonHomeCatalog

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,22 +1,27 @@
 import Carousel from "../components/carousel";
-import SmallCatalog from "../components/smallCatalog";
+import ButtonHomeCatalog from "../components/buttonHomeCatalog";
+// import SmallCatalog from "../components/smallCatalog";
 
 export default function Home() {
 	return (
 		<>
 			<Carousel />
+			<div role="tablist" className="tabs tabs-lift">
+				<ButtonHomeCatalog />
+				<section className="grid grid-cols-5 gap-4 justify-items-center tab-content bg-base-100 border-base-300 p-6">
+					{/* <SmallCatalog />
+					<SmallCatalog />
+					<SmallCatalog />
+					<SmallCatalog />
+					<SmallCatalog />
+					<SmallCatalog />
+					<SmallCatalog /> */}
+					{window.location.hash === "#hot" ? "HOT" : "NOT HOT"}
+				</section>
+			</div>
 			{/* <button className="btn">one-time</button>
 			<button className="btn">subscription</button>
 			<button className="btn">Hot</button> */}
-			<section className="grid grid-cols-5 gap-4 justify-items-center">
-				<SmallCatalog />
-				<SmallCatalog />
-				<SmallCatalog />
-				<SmallCatalog />
-				<SmallCatalog />
-				<SmallCatalog />
-				<SmallCatalog />
-			</section>
 		</>
 	);
 }
