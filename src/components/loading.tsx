fa68ac9da2ccb120c273e709ae46c4c1f704914a
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-06-27 17:50:08 +0700

    feat: starting fresh

--- src/components/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-	return (
-		<>
-			<section className="w-screen h-screen flex justify-center">
-				<span className="self-center loading loading-spinner loading-xl"></span>
-			</section>
-		</>
-	);
-}
--- src/pages/notFound.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-	return (
-		<>
-			<h1>Pages Not Found</h1>
-		</>
-	);
-}
