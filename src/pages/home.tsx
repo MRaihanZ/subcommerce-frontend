b286b85d9547d875138dfb25eb7bcf59ba62464c
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-06-30 06:13:19 +0700

    feat: wraping card inside section tag and add margin top

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -33,32 +33,34 @@ export default function Home() {
 				<CarouselPrevious />
 				<CarouselNext />
 			</Carousel>
-			<Card className="w-full max-w-3xs">
-				<CardHeader>
-					<CardTitle>
-						<img src="/assets/img/item.jpg" alt="" />
-					</CardTitle>
-					<CardDescription className="flex">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							height="24px"
-							viewBox="0 -960 960 960"
-							width="24px"
-							fill="currentColor"
-						>
-							<path d="M160-720v-80h640v80H160Zm0 560v-240h-40v-80l40-200h640l40 200v80h-40v240h-80v-240H560v240H160Zm80-80h240v-160H240v160Zm-38-240h556-556Zm0 0h556l-24-120H226l-24 120Z" />
-						</svg>
-						<span className="self-center">Indo Server</span>
-					</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<p>VPS Linux Indonesia</p>
-					<p className="text-xl font-bold">Rp64.000</p>
-				</CardContent>
-				<CardFooter>
-					<p>⭐ 4.5 | 100 terjual</p>
-				</CardFooter>
-			</Card>
+			<section className="mt-5">
+				<Card className="w-full max-w-3xs">
+					<CardHeader>
+						<CardTitle>
+							<img src="/assets/img/item.jpg" alt="" />
+						</CardTitle>
+						<CardDescription className="flex">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="24px"
+								viewBox="0 -960 960 960"
+								width="24px"
+								fill="currentColor"
+							>
+								<path d="M160-720v-80h640v80H160Zm0 560v-240h-40v-80l40-200h640l40 200v80h-40v240h-80v-240H560v240H160Zm80-80h240v-160H240v160Zm-38-240h556-556Zm0 0h556l-24-120H226l-24 120Z" />
+							</svg>
+							<span className="self-center">Indo Server</span>
+						</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<p>VPS Linux Indonesia</p>
+						<p className="text-xl font-bold">Rp64.000</p>
+					</CardContent>
+					<CardFooter>
+						<p>⭐ 4.5 | 100 terjual</p>
+					</CardFooter>
+				</Card>
+			</section>
 		</>
 	);
 }
