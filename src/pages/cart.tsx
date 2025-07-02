5b1d41743c9b76510d4f21444f2465d95f55f917
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-07-02 11:21:39 +0700

    feat: Add responsive layout for mobile view on cart pages

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,19 +1,37 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
 import CartItem from "@/components/my_components/cartItem";
 export default function Cart() {
 	return (
 		<>
-			<section className="grid grid-cols-7">
-				<section className="col-span-5 col-start-2">
+			<section className="grid grid-cols-12">
+				<section className="col-span-12 lg:col-span-6 lg:col-start-2 lg:mb-5">
 					<p className="font-semibold text-3xl">Keranjang</p>
 					<section className="mt-5">
-						<section className="flex ms-3 mb-3">
-							<Checkbox className="border-black" id="allItem" />
-							<Label htmlFor="allItem" className="ms-3">
-								Pilih Semua
-							</Label>
+						<section className="flex justify-between items-center  ms-3 mb-3">
+							<section className="flex">
+								<Checkbox className="border-black" id="allItem" />
+								<Label htmlFor="allItem" className="ms-3">
+									Pilih Semua
+								</Label>
+							</section>
+							<Button
+								variant="outline"
+								className="w-fit cursor-pointer hover:bg-red-600 px-3"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									height="24px"
+									viewBox="0 -960 960 960"
+									width="24px"
+									fill="currentColor"
+								>
+									<path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+								</svg>
+								Hapus Semua
+							</Button>
 						</section>
 						<section className="">
 							<CartItem />
@@ -23,12 +41,18 @@ export default function Cart() {
 							<CartItem />
 						</section>
 					</section>
-					<section className="justify-between flex ms-3 bottom-0 bg-white border">
-						<section className="flex justify-between">
-							<p>Total</p>
-							<p>Rp240.000</p>
+				</section>
+				<section className="col-span-12 lg:col-span-4 ms-3 mb-5 lg:ms-5 mt-5 lg:mt-28">
+					<section className="py-7 px-5 bottom-0 bg-white border rounded-md">
+						<section className="mb-5">
+							<p className="font-semibold text-xl pb-5">Ringkasan Belanja</p>
+							<Separator />
 						</section>
-						<Button className="bg-green-600">Beli</Button>
+						<section className="flex justify-between mb-5">
+							<p className="font-bold text-xl">Total</p>
+							<p className="font-bold text-xl">Rp240.000</p>
+						</section>
+						<Button className="bg-green-600 w-full cursor-pointer">Beli</Button>
 					</section>
 				</section>
 			</section>
