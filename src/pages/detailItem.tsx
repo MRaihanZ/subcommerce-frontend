2374dc1daa50be4bc2c1d576e05232a3de528c19
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-07-07 14:55:08 +0700

    feat: re-arrange component

--- src/pages/detailItem.tsx
+++ src/pages/detailItem.tsx
@@ -65,7 +65,7 @@ export default function DetailItem() {
 							</Avatar>
 							<p className="ms-3 font-bold text-xl">User Name</p>
 						</section>
-						<section className="flex justify-between mt-3 ms-11">
+						<section className="flex justify-between my-3 ms-11">
 							<section className="flex items-center">
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
@@ -80,7 +80,8 @@ export default function DetailItem() {
 							</section>
 							<p>⭐ 4.5</p>
 						</section>
-						<section className="flex justify-between mt-3 ms-11">
+						<Separator />
+						<section className="flex justify-between mt-3">
 							<Button
 								variant="outline"
 								className="cursor-pointer"
@@ -132,22 +133,7 @@ export default function DetailItem() {
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
 								finibus turpis a venenatis eleifend.
 							</p>
-							<section className="flex justify-between mt-3">
-								<section className="flex items-center">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										height="24px"
-										viewBox="0 -960 960 960"
-										width="24px"
-										fill="currentColor"
-									>
-										<path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
-									</svg>
-									<p className="ms-2">53 terjual</p>
-								</section>
-								<p>⭐ 4.5</p>
-							</section>
-							<p className="mt-3 font-bold text-xl">Rp64.000</p>
+							{/* <p className="mt-3 font-bold text-xl">Rp64.000</p> */}
 						</section>
 						<Separator />
 						<section className="mt-3">
@@ -183,6 +169,22 @@ export default function DetailItem() {
 									</section>
 								))}
 							</section>
+						</section>
+						<Separator />
+						<section className="flex justify-between my-3">
+							<section className="flex items-center">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									height="24px"
+									viewBox="0 -960 960 960"
+									width="24px"
+									fill="currentColor"
+								>
+									<path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
+								</svg>
+								<p className="ms-2">53 terjual</p>
+							</section>
+							<p>⭐ 4.5</p>
 						</section>
 						<Separator />
 						<section className="mt-3">
