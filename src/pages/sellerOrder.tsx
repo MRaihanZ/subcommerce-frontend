a85494858905d5ee3c6fa71ed50ade776bf1d41b
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-07-18 15:03:46 +0700

    feat: change naming from order to pesanan

--- src/pages/sellerOrder.tsx
+++ src/pages/sellerOrder.tsx
@@ -6,7 +6,7 @@ export default function SellerOrder() {
 		<>
 			<section className="grid grid-cols-12">
 				<section className="col-span-12 lg:col-span-8 lg:col-start-3">
-					<p className="font-semibold text-2xl">Order</p>
+					<p className="font-semibold text-2xl">Pesanan</p>
 					<section className="mt-5">
 						<section className="mb-3">
 							<section className="border rounded-md">
