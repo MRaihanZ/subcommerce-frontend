6db9f3669d42d3aca42a5ba1ce54f436f1451a92
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-07-20 12:56:32 +0700

    feat: change layout UI in profile page, change calendar type, and add responsive

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -4,7 +4,6 @@ import { Separator } from "@/components/ui/separator";
 import { Label } from "@/components/ui/label";
 import InputFormProfile from "@/components/my_components/inputFormProfile";
 
-import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -28,120 +27,119 @@ export default function Profile() {
 	const [isEditProfile, setIsEditProfile] = useState(false);
 	return (
 		<>
-			<section className="grid grid-cols-5">
-				<section className="col-span-3 col-start-2 border rounded-2xl">
+			<section className="grid lg:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-9 mt-7">
+				<section className="lg:col-span-3 xl:col-span-3 2xl:col-span-3 lg:col-start-2 xl:col-start-3 2xl:col-start-4 border rounded-2xl">
 					<p className="text-center py-3">Bio Data</p>
 					<Separator />
-					<section className="grid grid-cols-2">
-						<section className="ms-5">
-							<InputFormProfile
-								labelName="Nama"
-								id="nama"
-								name="nama"
-								type="text"
-								value={nama}
-								setValue={setNama}
-								isDisabled={isDisableButton}
-							/>
-							<InputFormProfile
-								labelName="Email"
-								id="email"
-								name="email"
-								type="email"
-								value={email}
-								setValue={setEmail}
-								isDisabled={isDisableButton}
-							/>
-							<section className="flex flex-col mt-5">
-								<Label htmlFor="date" className="mb-1">
-									Date of birth
-								</Label>
-								<Popover open={open} onOpenChange={setOpen}>
-									<PopoverTrigger id="date" asChild>
-										<Button
-											disabled={isDisableButton}
-											variant="outline"
-											data-empty={!date}
-											className="data-[empty=true]:text-muted-foreground w-[280px] justify-start text-left font-normal"
-										>
-											<CalendarIcon />
-											{date ? format(date, "PPP") : <span>10/03/2000</span>}
-										</Button>
-									</PopoverTrigger>
-									<PopoverContent className="w-auto p-0">
-										<Calendar
-											mode="single"
-											selected={date}
-											onSelect={setDate}
-											disabled={(date) =>
-												date > new Date() || date < new Date("1900-01-01")
-											}
-										/>
-									</PopoverContent>
-								</Popover>
-							</section>
-							<section className={isDisableButton ? "hidden mt-5" : "mt-5"}>
-								<Button
-									className={
-										isDisableButton
-											? "hidden cursor-pointer w-full"
-											: "cursor-pointer w-full"
-									}
+					<section className="place-self-center mt-5 p-3 border rounded-xl">
+						<img src="/assets/img/item.jpg" alt="" className="w-60 h-60" />
+					</section>
+					<section className="mx-5 mb-5">
+						<section
+							id="profile_picture"
+							className={
+								isEditProfile
+									? "grid w-full items-center mt-5"
+									: "hidden w-full items-center mt-5"
+							}
+						>
+							<Label htmlFor="picture" className="mb-1">
+								Picture
+							</Label>
+							<Input id="picture" type="file" />
+						</section>
+						<InputFormProfile
+							labelName="Nama"
+							id="nama"
+							name="nama"
+							type="text"
+							value={nama}
+							setValue={setNama}
+							isDisabled={isDisableButton}
+						/>
+						<InputFormProfile
+							labelName="Email"
+							id="email"
+							name="email"
+							type="email"
+							value={email}
+							setValue={setEmail}
+							isDisabled={isDisableButton}
+						/>
+						<section className="flex flex-col mt-5">
+							<Label htmlFor="date" className="mb-1">
+								Tanggal Lahir
+							</Label>
+							<Popover open={open} onOpenChange={setOpen}>
+								<PopoverTrigger asChild>
+									<Button
+										disabled={isDisableButton}
+										variant="outline"
+										id="date"
+										className="w-full justify-between font-normal"
+									>
+										{date ? date.toLocaleDateString() : "Pilih Tanggal"}
+										<CalendarIcon />
+									</Button>
+								</PopoverTrigger>
+								<PopoverContent
+									className="w-auto overflow-hidden p-0"
+									align="start"
 								>
-									Submit
-								</Button>
-							</section>
-							<section className={isEditPassword ? "mt-5" : "hidden mt-5"}>
-								<Label htmlFor="password" className="mb-1">
-									password
-								</Label>
-								<Input
-									id="{id}"
-									name="password"
-									type="password"
-									placeholder="********"
-								/>
-							</section>
-							<section
-								id="profile_picture"
+									<Calendar
+										mode="single"
+										selected={date}
+										captionLayout="dropdown"
+										onSelect={(date) => {
+											setDate(date);
+											setOpen(false);
+										}}
+										className="mx-auto"
+									/>
+								</PopoverContent>
+							</Popover>
+						</section>
+						<section className={isEditPassword ? "mt-5" : "hidden mt-5"}>
+							<Label htmlFor="password" className="mb-1">
+								password
+							</Label>
+							<Input
+								id="{id}"
+								name="password"
+								type="password"
+								placeholder="********"
+							/>
+						</section>
+						<section className={isDisableButton ? "hidden mt-5" : "mt-5"}>
+							<Button
 								className={
-									isEditProfile
-										? "grid w-full max-w-sm items-center mt-5"
-										: "hidden w-full max-w-sm items-center mt-5"
+									isDisableButton
+										? "hidden cursor-pointer w-full"
+										: "cursor-pointer w-full"
 								}
 							>
-								<Label htmlFor="picture" className="mb-1">
-									Picture
-								</Label>
-								<Input id="picture" type="file" />
-							</section>
-							<section className="flex justify-around mt-5">
-								<Button
-									className="cursor-pointer"
-									onClick={() => setIsDisableButton((prev) => !prev)}
-								>
-									Edit
-								</Button>
-								<Button
-									className="cursor-pointer"
-									onClick={() => setIsEditPassword((prev) => !prev)}
-								>
-									Change Password
-								</Button>
-								<Button
-									className="cursor-pointer"
-									onClick={() => setIsEditProfile((prev) => !prev)}
-								>
-									Change Profile Picture
-								</Button>
-							</section>
+								Submit
+							</Button>
 						</section>
-						<section className="place-self-center">
-							<img
-								src="/assets/img/item.jpg"
-								alt=""
-								className="w-60 h-60 ms-3"
-							/>
+						<section className="flex justify-around flex-col 2xl:flex-row gap-5 mt-5">
+							<Button
+								className="cursor-pointer"
+								onClick={() => setIsDisableButton((prev) => !prev)}
+							>
+								{isDisableButton ? "Edit" : "Batalkan"}
+							</Button>
+							<Button
+								className="cursor-pointer"
+								onClick={() => setIsEditPassword((prev) => !prev)}
+							>
+								Change Password
+							</Button>
+							<Button
+								className="cursor-pointer"
+								onClick={() => setIsEditProfile((prev) => !prev)}
+							>
+								{isEditProfile ? "Batalkan" : "Change Profile Picture"}
+							</Button>
 						</section>
 					</section>
 				</section>
