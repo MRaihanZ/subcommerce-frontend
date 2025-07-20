8f8cd6535f6f314f97e2a912a2aceba627e2eed7
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-07-20 17:06:32 +0700

    feat: add temporary layout for product seller

--- src/pages/sellerItem.tsx
+++ src/pages/sellerItem.tsx
@@ -1,7 +1,59 @@
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+	// TableFooter,
+} from "@/components/ui/table";
+
 export default function SellerItem() {
 	return (
 		<>
-			<p>Seller Item Pages</p>
+			<section className="mt-7">
+				<section className="my-7">
+					<Table className="">
+						<TableCaption>Barang</TableCaption>
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-[100px]">Id</TableHead>
+								<TableHead>Nama</TableHead>
+								<TableHead>Status</TableHead>
+								<TableHead>Tanggal Pembelian</TableHead>
+								<TableHead>Payment Id</TableHead>
+								<TableHead className="text-right">Harga</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							<TableRow className="h-20">
+								<TableCell className="font-medium">INV001</TableCell>
+								<TableCell>VPS Linux Indonesia</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell>2024-03-07 23:19</TableCell>
+								<TableCell>PAY001</TableCell>
+								<TableCell className="text-right">Rp150.000</TableCell>
+							</TableRow>
+							<TableRow className="h-20">
+								<TableCell className="font-medium">INV001</TableCell>
+								<TableCell>VPS Linux Indonesia</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell>2024-03-07 23:19</TableCell>
+								<TableCell>PAY001</TableCell>
+								<TableCell className="text-right">Rp150.000</TableCell>
+							</TableRow>
+						</TableBody>
+						{/* <TableFooter>
+							<TableRow>
+								<TableCell colSpan={5} className="text-right">
+									Total: Rp750.000
+								</TableCell>
+							</TableRow>
+						</TableFooter> */}
+					</Table>
+				</section>
+			</section>
 		</>
 	);
 }
