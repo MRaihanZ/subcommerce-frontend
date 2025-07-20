4092275381ec6b83e8e1d5de5576a1623341bc47
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-07-20 14:32:43 +0700

    feat: delete hapus akun button in seller page, and add onClick function navigate to seller profile page

--- src/pages/seller.tsx
+++ src/pages/seller.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router";
+
 import {
 	Card,
 	// CardAction,
@@ -27,6 +29,7 @@ import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 
 export default function Seller() {
+	const navigate = useNavigate();
 	return (
 		<>
 			<section className="mt-10">
@@ -71,7 +74,7 @@ export default function Seller() {
 						<Button
 							variant="outline"
 							className="cursor-pointer"
-							// onClick={}
+							onClick={() => navigate("/seller/profile")}
 						>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -83,25 +86,6 @@ export default function Seller() {
 								<path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
 							</svg>
 							Edit Seller Profile
-						</Button>
-						<section className="h-7 flex justify-center">
-							<Separator orientation="vertical" />
-						</section>
-						<Button
-							variant="destructive"
-							className="cursor-pointer"
-							// onClick={}
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								height="24px"
-								viewBox="0 -960 960 960"
-								width="24px"
-								fill="currentColor"
-							>
-								<path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-							</svg>
-							Delete Seller Account
 						</Button>
 					</section>
 				</section>
