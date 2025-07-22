f3582160e3ab3c11ed37e624b51553f3672c03e2
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-07-22 09:29:21 +0700

    feat: add responsive in layout order list

--- src/pages/orderList.tsx
+++ src/pages/orderList.tsx
@@ -5,7 +5,7 @@ export default function OrderList() {
 	return (
 		<>
 			<section className="grid grid-cols-12">
-				<section className="col-span-12 lg:col-span-6 lg:col-start-2">
+				<section className="col-span-12 lg:col-span-8 lg:col-start-3">
 					<p className="font-semibold text-2xl">Pembelian Produk</p>
 					<section className="mt-5">
 						<section className="mb-3">
