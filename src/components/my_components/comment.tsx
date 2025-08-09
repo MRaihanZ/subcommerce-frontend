ac53046a1f3c3ace3842f4c582c24762e36e52c9
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-08-10 02:08:53 +0700

    feat: add fetching seller, rating and comments in detail product page

--- src/components/my_components/comment.tsx
+++ src/components/my_components/comment.tsx
@@ -1,50 +1,96 @@
+import { useState, useEffect } from "react";
+
 import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
 
-export default function Comment() {
+interface CommentProps {
+	prodId: string | null;
+}
+interface RatingComments {
+	u_name: string;
+	img: string;
+	pv_name: string;
+	interval: number;
+	i_name: string;
+	rating: number;
+	comment: string;
+	created_at: string;
+}
+
+export default function Comment({ prodId }: CommentProps) {
+	const [ratingComments, setRatingComments] = useState<RatingComments[] | null>(
+		null
+	);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string>();
+	useEffect(() => {
+		const prodNumParam = Number(prodId);
+		const fetchComments = async () => {
+			try {
+				const res = await fetch(
+					"http://localhost:8080/api/v1/ratings/comments/" + prodNumParam
+				);
+				const json = await res.json();
+				if (json.code === 200 && json.status === "ok") {
+					setRatingComments(json.data);
+					setLoading(false);
+				} else {
+					setError(json.error);
+					setLoading(false);
+				}
+			} catch (err) {
+				const errFetch = "Network Error: " + err;
+				setError(errFetch);
+				setLoading(false);
+			}
+		};
+		fetchComments();
+	}, [prodId]);
+	if (loading) return <p>Loading...</p>;
+	if (error) {
+		return <p>{error}</p>;
+	}
 	return (
 		<>
-			<section className="mt-3 pb-5">
-				<section className="flex items-center mt-3">
-					<section className="flex">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							height="24px"
-							viewBox="0 -960 960 960"
-							width="24px"
-							fill="currentColor"
-						>
-							<path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
-						</svg>
-						<p>username</p>
+			{ratingComments?.map((r, i) => (
+				<>
+					<section key={i} className="mt-3 pb-5">
+						<section className="flex items-center mt-3">
+							<section className="flex">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									height="24px"
+									viewBox="0 -960 960 960"
+									width="24px"
+									fill="currentColor"
+								>
+									<path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
+								</svg>
+								<p>{r.u_name}</p>
+							</section>
+							<p className="mx-2">|</p>
+							<p className="font-semibold text-lg">⭐ {r.rating}</p>
+						</section>
+						<p className="text-xs ms-6.5">{r.created_at}</p>
+						<p className="mt-5 mb-1">
+							Varian:
+							<Badge variant="outline" className="mx-1">
+								{r.pv_name}
+							</Badge>
+						</p>
+						<p className="mb-5">
+							Periode:
+							<Badge variant="outline" className="mx-1">
+								{r.interval} {r.i_name}
+							</Badge>
+						</p>
+						<section>
+							<p>{r.comment}</p>
+						</section>
 					</section>
-					<p className="mx-2">|</p>
-					<p className="font-semibold text-lg">⭐ 4</p>
-				</section>
-				<p className="text-xs ms-6.5">2025-07-19 09.00</p>
-				<p className="my-5">
-					variant:
-					<Badge variant="outline" className="mx-1">
-						1 Core 512MB
-					</Badge>
-				</p>
-				<section>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-						lobortis tempor lacus, et hendrerit orci viverra ut. Nullam
-						convallis neque dignissim leo venenatis, a semper elit sollicitudin.
-						Donec aliquet, magna ac efficitur commodo, risus orci hendrerit
-						massa, ac ultricies massa lacus in arcu. Nunc id gravida est. Donec
-						ac blandit nibh. Cras leo ex, imperdiet a nisl in, tincidunt
-						tincidunt ante. Morbi semper viverra tincidunt. Quisque erat lectus,
-						accumsan nec imperdiet sollicitudin, cursus at arcu. Nullam aliquet
-						consectetur orci et condimentum. Vestibulum sit amet purus
-						porttitor, volutpat dui feugiat, accumsan lorem. Vivamus congue ac
-						nulla porta faucibus. Maecenas efficitur mauris eu sodales dictum.
-					</p>
-				</section>
-			</section>
-			<Separator />
+					<Separator key={i} />
+				</>
+			))}
 		</>
 	);
 }
--- src/pages/detailItem.tsx
+++ src/pages/detailItem.tsx
@@ -53,6 +53,19 @@ interface ProductDetail {
 	product_variants: ProductVariant[];
 }
 
+interface SellerSummarize {
+	id: string;
+	name: string;
+	img: string;
+	sold_products: number;
+	average_rating: number;
+}
+
+interface Rating {
+	average_rating: number;
+	rating_count: number;
+}
+
 export default function DetailItem() {
 	// const selectedLength = Array(rawData.variantTitle.length).fill("");
 	const [variantState, setVariantState] = useState("");
@@ -74,6 +87,9 @@ export default function DetailItem() {
 
 	const prodParam = searchParams.get("product");
 	const prodVarParam = searchParams.get("variant");
+
+	const [seller, setSeller] = useState<SellerSummarize | null>(null);
+	const [rating, setRating] = useState<Rating | null>(null);
 
 	useEffect(() => {
 		// Products
@@ -110,8 +126,48 @@ export default function DetailItem() {
 		fetchProduct();
 
 		// seller
+		const fetchSeller = async () => {
+			try {
+				const res = await fetch(
+					"http://localhost:8080/api/v1/sellers/" + prodParam + "/summarize"
+				);
+				const json = await res.json();
+				if (json.code === 200 && json.status === "ok") {
+					setSeller(json.data);
+					setLoading(false);
+				} else {
+					setError(json.error);
+					setLoading(false);
+				}
+			} catch (err) {
+				const errFetch = "Network Error: " + err;
+				setError(errFetch);
+				setLoading(false);
+			}
+		};
+		fetchSeller();
 
-		// comment
+		// rating
+		const fetchRating = async () => {
+			try {
+				const res = await fetch(
+					"http://localhost:8080/api/v1/ratings/" + prodParam
+				);
+				const json = await res.json();
+				if (json.code === 200 && json.status === "ok") {
+					setRating(json.data);
+					setLoading(false);
+				} else {
+					setError(json.error);
+					setLoading(false);
+				}
+			} catch (err) {
+				const errFetch = "Network Error: " + err;
+				setError(errFetch);
+				setLoading(false);
+			}
+		};
+		fetchRating();
 	}, [prodParam]);
 	useEffect(() => {
 		if (product !== null) {
@@ -215,10 +271,10 @@ export default function DetailItem() {
 					<section className="mt-5 border rounded-md px-10 py-3">
 						<section className="flex items-center ">
 							<Avatar>
-								<AvatarImage src="/assets/img/profile1.jpg" />
-								<AvatarFallback>Profile1</AvatarFallback>
+								<AvatarImage src={seller?.img} />
+								<AvatarFallback>Image Profile Seller</AvatarFallback>
 							</Avatar>
-							<p className="ms-3 font-bold text-xl">User Name</p>
+							<p className="ms-3 font-bold text-xl">{seller?.name}</p>
 						</section>
 						<section className="flex justify-between my-3 ms-11">
 							<section className="flex items-center">
@@ -231,9 +287,9 @@ export default function DetailItem() {
 								>
 									<path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
 								</svg>
-								<p className="ms-2">500+ Barang Terjual</p>
+								<p className="ms-2">{seller?.sold_products} Barang Terjual</p>
 							</section>
-							<p>⭐ 4.5</p>
+							<p>⭐ {Math.floor(seller?.average_rating * 10) / 10}</p>
 						</section>
 						<Separator />
 						<section className="flex justify-between mt-3">
@@ -241,7 +297,7 @@ export default function DetailItem() {
 								variant="outline"
 								className="cursor-pointer"
 								onClick={() => {
-									navigate("/chat");
+									navigate("/chat?seller=" + seller?.id);
 								}}
 							>
 								<svg
@@ -497,7 +553,7 @@ export default function DetailItem() {
 					<section className="flex justify-center gap-3 border rounded-md px-7 py-5">
 						<section>
 							<p className="font-semibold text-3xl">
-								⭐ {Math.floor(product.average_rating * 10) / 10}
+								⭐ {Math.floor(rating?.average_rating * 10) / 10}
 								<sub className="text-base"> / 5.0</sub>
 							</p>
 							<section className="flex ms-1.5 mt-3">
@@ -510,7 +566,7 @@ export default function DetailItem() {
 								>
 									<path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
 								</svg>
-								<p className="ms-3">{product.rating_count} Pembeli</p>
+								<p className="ms-3">{rating?.rating_count} Pembeli</p>
 							</section>
 						</section>
 					</section>
@@ -518,13 +574,7 @@ export default function DetailItem() {
 				<section className="col-span-8 col-start-4 ms-5">
 					<section className="border rounded-md p-5">
 						<p className="mb-5 text-xl font-semibold">Ulasan Pembeli</p>
-						<Comment />
-						<Comment />
-						<Comment />
-						<Comment />
-						<Comment />
-						<Comment />
-						<Comment />
+						<Comment prodId={prodParam} />
 					</section>
 				</section>
 			</section>
