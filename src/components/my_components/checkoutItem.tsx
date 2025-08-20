7fe610c7684c820e73562beb22226d92e08fd719
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-08-20 22:16:32 +0700

    feat: request data checkout and add function for add to order

--- src/components/my_components/checkoutItem.tsx
+++ src/components/my_components/checkoutItem.tsx
@@ -1,52 +1,118 @@
+import { useState, useEffect } from "react";
+
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export default function CheckoutItem() {
+interface CheckoutProduct {
+	p_id: number;
+	pv_id: number;
+	s_name: string;
+	s_img: string;
+	p_name: string;
+	p_img: string;
+	pv_name: string;
+	quantity: number;
+	total_price: number;
+}
+
+interface OrderProduct {
+	pay_id: number;
+	p_id: number;
+	pv_id: number;
+	note: string;
+	quantity: number;
+	unit_price: number;
+	total_price: number;
+}
+
+interface CheckoutItemProps {
+	data: CheckoutProduct;
+	index: number;
+	orderFunc: React.Dispatch<React.SetStateAction<OrderProduct[]>>;
+	selectedPaymentId: number;
+}
+
+export default function CheckoutItem({
+	data,
+	index,
+	orderFunc,
+	selectedPaymentId,
+}: CheckoutItemProps) {
+	const [note, setNote] = useState<string>("");
+	useEffect(() => {
+		orderFunc((prev) => {
+			const newOrder = [...prev];
+			newOrder[index] = {
+				pay_id: selectedPaymentId,
+				p_id: data.p_id,
+				pv_id: data.pv_id,
+				note: note,
+				quantity: data.quantity,
+				unit_price: data.total_price / data.quantity,
+				total_price: data.total_price,
+			};
+			return newOrder;
+		});
+	}, [note, data, selectedPaymentId]);
 	return (
 		<>
 			<section className="ms-3 py-5 px-5">
 				<section className="flex items-center">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24px"
-						viewBox="0 -960 960 960"
-						width="24px"
-						fill="currentColor"
-					>
-						<path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
-					</svg>
-					<p className="ms-3 font-semibold">User Name</p>
+					<Avatar>
+						<AvatarImage src={data.s_img} />
+						<AvatarFallback>Image Profile Seller</AvatarFallback>
+					</Avatar>
+					<p className="ms-3 font-semibold">{data.s_name}</p>
 				</section>
 				<section className="flex">
 					<section className="flex-none my-auto">
-						<img src="/assets/img/item.jpg" alt="" className="w-40 h-40 ms-3" />
+						<img src={data.p_img} alt="" className="w-40 h-40 ms-3" />
 					</section>
 					<section className="flex-auto ms-5">
 						<p className="my-3 text-lg">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-							finibus turpis a venenatis eleifend.
+							{data.p_name}
+							{data.pv_name === "default" ? "" : " - " + data.pv_name}
 						</p>
-						<p>
-							variant:
-							<Badge variant="outline" className="mx-1">
-								2 Core 4GB
-							</Badge>
-						</p>
+						{data.pv_name === "default" ? (
+							""
+						) : (
+							<p>
+								variant:
+								<Badge variant="outline" className="mx-1">
+									{data.pv_name}
+								</Badge>
+							</p>
+						)}
 						<p>
 							Jumlah:
 							<Badge variant="outline" className="ms-1">
-								1
+								{data.quantity}
 							</Badge>
 						</p>
-						<p>Harga Total: Rp64.000</p>
+						<p>
+							Harga Total: Rp
+							{new Intl.NumberFormat("id-ID").format(data.total_price)}
+						</p>
 						<section className="mt-5">
-							<Label htmlFor="note">Catatan</Label>
-							<Input id="note" name="note" type="text" placeholder="..." />
+							<Label htmlFor="note" className="mb-1">
+								Catatan
+							</Label>
+							<Input
+								id="note"
+								name="note"
+								type="text"
+								placeholder="..."
+								value={note ?? ""}
+								onChange={(e) => setNote(e.target.value)}
+							/>
 						</section>
 					</section>
 				</section>
 			</section>
+			<Separator />
 		</>
 	);
 }
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+
+import { GetCsrf } from "@/components/utils/csrf";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -10,6 +13,7 @@ import {
 	CommandInput,
 	CommandItem,
 	CommandList,
+	CommandSeparator,
 } from "@/components/ui/command";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import {
@@ -17,40 +21,183 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from "@/components/ui/popover";
+import { toast } from "sonner";
 
 import CheckoutItem from "@/components/my_components/checkoutItem";
 
-type Status = {
-	value: string;
-	label: string;
-};
-const statuses: Status[] = [
-	{
-		value: "qris",
-		label: "Qris",
-	},
-	{
-		value: "bsi",
-		label: "BSI",
-	},
-	{
-		value: "mandiri",
-		label: "Mandiri",
-	},
-	{
-		value: "bCA",
-		label: "BCA",
-	},
-	{
-		value: "BNI",
-		label: "BNI",
-	},
-];
+interface Payment {
+	p_id: number;
+	p_name: string;
+	category_payment_id: number;
+	cp_name: string;
+}
+
+interface CheckoutProduct {
+	p_id: number;
+	pv_id: number;
+	s_name: string;
+	s_img: string;
+	p_name: string;
+	p_img: string;
+	pv_name: string;
+	quantity: number;
+	total_price: number;
+}
+
+interface OrderProduct {
+	pay_id: number;
+	p_id: number;
+	pv_id: number;
+	note: string;
+	quantity: number;
+	unit_price: number;
+	total_price: number;
+}
+
+interface PaymentListsProps {
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+	payments: Payment[] | null;
+	categoryPayments: string[] | null;
+	setSelectedPaymentId: React.Dispatch<React.SetStateAction<number>>;
+	setSelectedPaymentName: React.Dispatch<React.SetStateAction<string | null>>;
+	setPaymentPrice: React.Dispatch<React.SetStateAction<number>>;
+}
 
 export default function Checkout() {
 	const [open, setOpen] = useState(false);
 	const isDesktop = useMediaQuery("(min-width: 768px)");
-	const [selectedStatus, setSelectedStatus] = useState<Status | null>(null);
+	const [payments, setPayments] = useState<Payment[] | null>(null);
+	const [categoryPayments, setCategoryPayments] = useState<string[] | null>(
+		null
+	);
+	const [selectedPaymentId, setSelectedPaymentId] = useState<number>(0);
+	const [selectedPaymentName, setSelectedPaymentName] = useState<string | null>(
+		null
+	);
+
+	const [checkout, setCheckout] = useState<CheckoutProduct[] | null>(null);
+	const servicePrice = 3000;
+	const [paymentPrice, setPaymentPrice] = useState<number>(0);
+	const [totalPriceProduct, setTotalPriceProduct] = useState<number>(0);
+	const [totalPrice, setTotalPrice] = useState<number>(0);
+
+	const [order, setOrder] = useState<OrderProduct[]>([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string>();
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		// Checkout
+		const fetchCheckout = async () => {
+			try {
+				const res = await fetch(
+					"http://localhost:8080/api/v1/orders/checkouts",
+					{
+						credentials: "include",
+					}
+				);
+				const json = await res.json();
+				if (json.code === 200 && json.status === "ok") {
+					toast.success("Berhasil mengambil data checkout");
+					setCheckout(json.data);
+					setLoading(false);
+				} else {
+					toast.error(json.error);
+					setError(json.error);
+					setLoading(false);
+				}
+			} catch (err) {
+				const errFetch = "Network Error: " + err;
+				toast.error(errFetch);
+				setError(errFetch);
+				setLoading(false);
+			}
+		};
+		fetchCheckout();
+
+		// Payments
+		const fetchPayments = async () => {
+			try {
+				const res = await fetch(
+					"http://localhost:8080/api/v1/orders/payments",
+					{
+						credentials: "include",
+					}
+				);
+				const json = await res.json();
+				if (json.code === 200 && json.status === "ok") {
+					setPayments(json.data);
+					setLoading(false);
+				} else {
+					toast.error(json.error);
+					setError(json.error);
+					setLoading(false);
+				}
+			} catch (err) {
+				const errFetch = "Network Error: " + err;
+				toast.error(errFetch);
+				setError(errFetch);
+				setLoading(false);
+			}
+		};
+		fetchPayments();
+	}, []);
+
+	useEffect(() => {
+		const allCategories = payments?.map((item) => item.cp_name);
+		const uniqueCategories = Array.from(new Set(allCategories));
+
+		setCategoryPayments(uniqueCategories);
+	}, [payments]);
+
+	useEffect(() => {
+		if (checkout) {
+			const total = checkout.reduce(
+				(total, price) => total + price.total_price,
+				0
+			);
+			setTotalPriceProduct(total);
+			setTotalPrice(total + servicePrice + paymentPrice);
+		}
+	}, [checkout, paymentPrice]);
+
+	const handleOrderSubmit = async () => {
+		if (!selectedPaymentName) {
+			toast.warning("Pilih jenis pembayaran terlebih dahulu");
+			return;
+		}
+		const csrfToken = await GetCsrf();
+
+		try {
+			const send = await fetch("http://localhost:8080/api/v1/orders/", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"X-CSRF-TOKEN": csrfToken,
+				},
+				credentials: "include",
+				body: JSON.stringify(order),
+			});
+
+			const result = await send.json();
+			if (result.code === 200 && result.status === "ok") {
+				navigate("/order-list");
+			} else {
+				toast.error(result.error);
+				setError(result.error);
+				// setLoading(false);
+			}
+		} catch (err) {
+			const errFetch = "Network Error: " + err;
+			setError(errFetch);
+		}
+	};
+	if (loading) return <p>Loading...</p>;
+	if (error) {
+		return <p>{error}</p>;
+	}
+	if (!checkout) return <p>Produk tidak ditemukan</p>;
 	return (
 		<>
 			<section className="grid grid-cols-12">
@@ -59,15 +206,15 @@ export default function Checkout() {
 					<section className="mt-5">
 						<section className="mb-3">
 							<section className="border rounded-md">
-								<CheckoutItem />
-								<Separator />
-								<CheckoutItem />
-								<Separator />
-								<CheckoutItem />
-								<Separator />
-								<CheckoutItem />
-								<Separator />
-								<CheckoutItem />
+								{checkout.map((item, index) => (
+									<CheckoutItem
+										key={index}
+										data={item}
+										index={index}
+										orderFunc={setOrder}
+										selectedPaymentId={selectedPaymentId}
+									/>
+								))}
 							</section>
 						</section>
 					</section>
@@ -86,17 +233,21 @@ export default function Checkout() {
 											variant="outline"
 											className="w-[15rem] justify-start"
 										>
-											{selectedStatus ? (
-												<>{selectedStatus.label}</>
+											{selectedPaymentName ? (
+												<>{selectedPaymentName}</>
 											) : (
 												<>Pilih Metode Pembayaran</>
 											)}
 										</Button>
 									</PopoverTrigger>
 									<PopoverContent className="w-[15rem] p-0" align="start">
-										<StatusList
+										<PaymentLists
 											setOpen={setOpen}
-											setSelectedStatus={setSelectedStatus}
+											payments={payments}
+											categoryPayments={categoryPayments}
+											setSelectedPaymentId={setSelectedPaymentId}
+											setSelectedPaymentName={setSelectedPaymentName}
+											setPaymentPrice={setPaymentPrice}
 										/>
 									</PopoverContent>
 								</Popover>
@@ -107,8 +258,8 @@ export default function Checkout() {
 											variant="outline"
 											className="w-[15rem] justify-start"
 										>
-											{selectedStatus ? (
-												<>{selectedStatus.label}</>
+											{selectedPaymentName ? (
+												<>{selectedPaymentName}</>
 											) : (
 												<>Pilih Metode Pembayaran</>
 											)}
@@ -116,9 +267,13 @@ export default function Checkout() {
 									</DrawerTrigger>
 									<DrawerContent>
 										<div className="mt-4 border-t">
-											<StatusList
+											<PaymentLists
 												setOpen={setOpen}
-												setSelectedStatus={setSelectedStatus}
+												payments={payments}
+												categoryPayments={categoryPayments}
+												setSelectedPaymentId={setSelectedPaymentId}
+												setSelectedPaymentName={setSelectedPaymentName}
+												setPaymentPrice={setPaymentPrice}
 											/>
 										</div>
 									</DrawerContent>
@@ -127,12 +282,16 @@ export default function Checkout() {
 						</section>
 						<section className="flex justify-between mb-2">
 							<p className="text-md">Total Harga</p>
-							<p className="text-md">Rp189.000</p>
+							<p className="text-md">
+								Rp{new Intl.NumberFormat("id-ID").format(totalPriceProduct)}
+							</p>
 						</section>
-						{selectedStatus ? (
+						{selectedPaymentName ? (
 							<section className="flex justify-between mb-2">
 								<p className="text-md">Biaya Admin</p>
-								<p className="text-md">Rp2.500</p>
+								<p className="text-md">
+									Rp{new Intl.NumberFormat("id-ID").format(paymentPrice)}
+								</p>
 							</section>
 						) : (
 							<></>
@@ -140,13 +299,20 @@ export default function Checkout() {
 
 						<section className="flex justify-between mb-2">
 							<p className="text-md">Biaya Jasa Aplikasi</p>
-							<p className="text-md">Rp3.000</p>
+							<p className="text-md">
+								Rp{new Intl.NumberFormat("id-ID").format(servicePrice)}
+							</p>
 						</section>
 						<section className="flex justify-between mb-5">
 							<p className="font-bold text-xl">Total Tagihan</p>
-							<p className="font-bold text-xl">Rp240.000</p>
+							<p className="font-bold text-xl">
+								Rp{new Intl.NumberFormat("id-ID").format(totalPrice)}
+							</p>
 						</section>
-						<Button className="bg-green-600 hover:bg-green-800 w-full cursor-pointer">
+						<Button
+							className="bg-green-600 hover:bg-green-800 w-full cursor-pointer"
+							onClick={handleOrderSubmit}
+						>
 							Beli
 						</Button>
 					</section>
@@ -156,34 +322,46 @@ export default function Checkout() {
 	);
 }
 
-function StatusList({
+function PaymentLists({
 	setOpen,
-	setSelectedStatus,
-}: {
-	setOpen: (open: boolean) => void;
-	setSelectedStatus: (status: Status | null) => void;
-}) {
+	payments,
+	categoryPayments,
+	setSelectedPaymentId,
+	setSelectedPaymentName,
+	setPaymentPrice,
+}: PaymentListsProps) {
 	return (
 		<Command>
 			<CommandInput placeholder="Filter status..." />
 			<CommandList>
 				<CommandEmpty>No results found.</CommandEmpty>
-				<CommandGroup>
-					{statuses.map((status) => (
-						<CommandItem
-							key={status.value}
-							value={status.value}
-							onSelect={(value) => {
-								setSelectedStatus(
-									statuses.find((priority) => priority.value === value) || null
-								);
-								setOpen(false);
-							}}
-						>
-							{status.label}
-						</CommandItem>
-					))}
-				</CommandGroup>
+				<CommandSeparator />
+				{categoryPayments?.map((vcp, i) => (
+					<section key={i}>
+						<CommandSeparator />
+						<CommandGroup heading={vcp}>
+							{payments?.map((vp, i) => (
+								<section key={i}>
+									{vcp === vp.cp_name ? (
+										<CommandItem
+											value={vp.p_name}
+											onSelect={(value) => {
+												setSelectedPaymentId(vp.p_id);
+												setSelectedPaymentName(value);
+												setPaymentPrice(2500);
+												setOpen(false);
+											}}
+										>
+											{vp.p_name}
+										</CommandItem>
+									) : (
+										""
+									)}
+								</section>
+							))}
+						</CommandGroup>
+					</section>
+				))}
 			</CommandList>
 		</Command>
 	);
