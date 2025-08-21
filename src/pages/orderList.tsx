020b717459c465cd777a743ab203bc04b5f2be66
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-08-21 22:48:08 +0700

    feat: add new interface for order data, fetch orders, iterate data and send the data to OrderListProduct component

--- src/pages/orderList.tsx
+++ src/pages/orderList.tsx
@@ -1,7 +1,66 @@
+import { useState, useEffect } from "react";
+
+import { toast } from "sonner";
 // import { Separator } from "@/components/ui/separator";
 
 import OrderListProduct from "@/components/my_components/orderListProduct";
+
+export interface OrderProducts {
+	o_id: number;
+	o_order_pretty_id: string;
+	pay_name: string;
+	os_name: string;
+	rating: boolean;
+	p_id: number;
+	pv_id: number;
+	s_name: string;
+	s_img: string;
+	p_name: string;
+	p_img: string;
+	active: string;
+	pv_name: string;
+	interval: number;
+	i_name: string;
+	quantity: number;
+	total_price: number;
+}
+
 export default function OrderList() {
+	const [order, setOrder] = useState<OrderProducts[] | null>(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string>();
+
+	useEffect(() => {
+		const fetchCheckout = async () => {
+			try {
+				const res = await fetch("http://localhost:8080/api/v1/orders/", {
+					credentials: "include",
+				});
+				const json = await res.json();
+				if (json.code === 200 && json.status === "ok") {
+					toast.success("Berhasil mengambil data order");
+					setOrder(json.data);
+					setLoading(false);
+				} else {
+					toast.error(json.error);
+					setError(json.error);
+					setLoading(false);
+				}
+			} catch (err) {
+				const errFetch = "Network Error: " + err;
+				toast.error(errFetch);
+				setError(errFetch);
+				setLoading(false);
+			}
+		};
+		fetchCheckout();
+	}, []);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) {
+		return <p>{error}</p>;
+	}
+	if (!order) return <p>Produk tidak ditemukan</p>;
 	return (
 		<>
 			<section className="grid grid-cols-12">
@@ -10,7 +69,9 @@ export default function OrderList() {
 					<section className="mt-5">
 						<section className="mb-3">
 							<section className="">
-								<OrderListProduct />
+								{order.map((item, index) => (
+									<OrderListProduct key={index} data={item} />
+								))}
 							</section>
 						</section>
 					</section>
