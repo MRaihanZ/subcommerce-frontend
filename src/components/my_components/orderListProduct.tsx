7518dad5579f6d6b6e18021ff09044a518b55099
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-08-21 22:51:35 +0700

    feat: add new interface for order data, displaying the data that being send, add new useState for rating and comment, add function in button for rating, add button with condition for cancel order and add function for handle submit rating and comment

--- src/components/my_components/orderListProduct.tsx
+++ src/components/my_components/orderListProduct.tsx
@@ -1,27 +1,106 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { GetCsrf } from "@/components/utils/csrf";
 
 import { Badge } from "@/components/ui/badge";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-// import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { toast } from "sonner";
 
-export default function OrderListProduct() {
+export interface OrderProducts {
+	o_id: number;
+	o_order_pretty_id: string;
+	pay_name: string;
+	os_name: string;
+	rating: boolean;
+	p_id: number;
+	pv_id: number;
+	s_name: string;
+	s_img: string;
+	p_name: string;
+	p_img: string;
+	active: string;
+	pv_name: string;
+	interval: number;
+	i_name: string;
+	quantity: number;
+	total_price: number;
+}
+
+interface OrderListProductProps {
+	data: OrderProducts;
+}
+
+export default function OrderListProduct({ data }: OrderListProductProps) {
 	const [penilaian, setPenilaian] = useState(false);
+	const [rate, setRate] = useState<number>(0);
+	const [comment, setComment] = useState<string>("");
+	const [error, setError] = useState<string>();
+
+	const handleOrderSubmit = async () => {
+		if (!rate) {
+			toast.warning("Pilih jenis pembayaran terlebih dahulu");
+			return;
+		}
+		const csrfToken = await GetCsrf();
+
+		const payload = {
+			rating: rate,
+			comment: comment,
+		};
+
+		try {
+			const send = await fetch(
+				"http://localhost:8080/api/v1/ratings/" +
+					data.p_id +
+					"/" +
+					data.pv_id +
+					"/" +
+					data.o_id,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						"X-CSRF-TOKEN": csrfToken,
+					},
+					credentials: "include",
+					body: JSON.stringify(payload),
+				}
+			);
+
+			const result = await send.json();
+			if (result.code === 200 && result.status === "ok") {
+				setPenilaian((prev) => !prev);
+				toast.success("Berhasil memberikan penilaian");
+			} else {
+				toast.error(result.error);
+				setError(result.error);
+				// setLoading(false);
+			}
+		} catch (err) {
+			const errFetch = "Network Error: " + err;
+			setError(errFetch);
+		}
+	};
+
+	if (error) {
+		return <p>{error}</p>;
+	}
 	return (
 		<>
 			<section className="ms-3 mt-5 py-5 px-5 border rounded-md">
-				<section className="flex items-center ms-6 mb-5">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24px"
-						viewBox="0 -960 960 960"
-						width="24px"
-						fill="currentColor"
-					>
-						<path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
-					</svg>
-					<p className="ms-3 font-semibold">User Name</p>
+				<section className="flex justify-between items-center ms-6 mb-5">
+					<section className="flex items-center">
+						<Avatar>
+							<AvatarImage src={data.s_img} />
+							<AvatarFallback>Image Profile Seller</AvatarFallback>
+						</Avatar>
+						<p className="ms-3 font-semibold">{data.s_name}</p>
+					</section>
+					<section>
+						<p>{data.o_order_pretty_id}</p>
+					</section>
 				</section>
 				<section className="flex">
 					<section className="flex-none my-auto">
@@ -29,55 +108,153 @@ export default function OrderListProduct() {
 					</section>
 					<section className="flex-auto ms-5">
 						<p className="my-3 text-lg">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-							finibus turpis a venenatis eleifend.
+							{data.p_name}
+							{data.pv_name === "default" ? "" : " - " + data.pv_name}
 						</p>
-						<p>
-							variant:
-							<Badge variant="outline" className="mx-1">
-								2 Core 4GB
-							</Badge>
-						</p>
-						<p>
-							Periode:
-							<Badge variant="outline" className="mx-1">
-								1 bulan
-							</Badge>
-						</p>
-						<p>
-							Jumlah:
-							<Badge variant="outline" className="ms-1">
-								1
-							</Badge>
-						</p>
-						<p>Harga Total: Rp64.000</p>
+						<section className="flex justify-between">
+							<section>
+								{data.pv_name === "default" ? (
+									""
+								) : (
+									<p>
+										variant:
+										<Badge variant="outline" className="mx-1">
+											{data.pv_name}
+										</Badge>
+									</p>
+								)}
+								<p>
+									Periode:
+									<Badge variant="outline" className="mx-1">
+										{data.interval + " " + data.i_name}
+									</Badge>
+								</p>
+								<p>
+									Jumlah:
+									<Badge variant="outline" className="ms-1">
+										{data.quantity}
+									</Badge>
+								</p>
+								<p>
+									Harga Total: Rp
+									{new Intl.NumberFormat("id-ID").format(data.total_price)}
+								</p>
+							</section>
+							<section>
+								<p>
+									pembayaran:
+									<Badge variant="default" className="ms-1">
+										{data.pay_name}
+									</Badge>
+								</p>
+								<p>
+									status:
+									<Badge variant="default" className="ms-1">
+										{data.os_name}
+									</Badge>
+								</p>
+							</section>
+						</section>
 					</section>
 				</section>
 				<section className="flex justify-end">
-					<Button
-						className="cursor-pointer mt-5"
-						onClick={() => setPenilaian((prev) => !prev)}
-					>
-						Beri Nilai
-					</Button>
+					{data.os_name === "menunggu pembayaran" ? (
+						<>
+							<Button
+								className="cursor-pointer mt-5"
+								variant="destructive"
+								onClick={() => setPenilaian((prev) => !prev)}
+							>
+								Batalkan pembayaran
+							</Button>
+							<Button
+								className="cursor-pointer mt-5 ms-3"
+								onClick={() => setPenilaian((prev) => !prev)}
+							>
+								(UNTUK SEMENTARA) Pembayaran selesai
+							</Button>
+						</>
+					) : (
+						""
+					)}
+					{data.os_name === "menunggu konfirmasi seller" ? (
+						<Button
+							className="cursor-pointer mt-5"
+							variant="destructive"
+							onClick={() => setPenilaian((prev) => !prev)}
+						>
+							Batalkan pesanan
+						</Button>
+					) : (
+						""
+					)}
+					{data.os_name === "produk sudah dikirim" ? (
+						<section className="mt-5">
+							<Button
+								className="cursor-pointer"
+								onClick={() => setPenilaian((prev) => !prev)}
+							>
+								Produk diterima
+							</Button>
+							<Button
+								className="cursor-pointer ms-3"
+								variant="destructive"
+								onClick={() => setPenilaian((prev) => !prev)}
+							>
+								Produk tidak diterima
+							</Button>
+						</section>
+					) : (
+						""
+					)}
+					{data.os_name === "pesanan selesai" && data.rating === false ? (
+						<Button
+							className="cursor-pointer mt-5"
+							onClick={() => setPenilaian((prev) => !prev)}
+						>
+							Beri Nilai
+						</Button>
+					) : (
+						""
+					)}
 				</section>
 				{penilaian ? (
 					<section className="mt-5">
 						<section className="flex items-center gap-3 mb-3">
 							<p>Beri Penilaian:</p>
-							<Button variant="outline" className="cursor-pointer">
+							<Button
+								variant={rate === 1 ? "default" : "outline"}
+								className="cursor-pointer"
+								onClick={() => setRate(1)}
+							>
 								⭐
 							</Button>
-							<Button variant="outline" className="cursor-pointer">
+							<Button
+								variant={rate === 2 ? "default" : "outline"}
+								className="cursor-pointer"
+								onClick={() => setRate(2)}
+							>
 								⭐⭐
 							</Button>
-							<Button variant="outline" className="cursor-pointer">
+							<Button
+								variant={rate === 3 ? "default" : "outline"}
+								className="cursor-pointer"
+								onClick={() => setRate(3)}
+							>
 								⭐⭐⭐
 							</Button>
-							<Button variant="outline" className="cursor-pointer">
+							<Button
+								variant={rate === 4 ? "default" : "outline"}
+								className="cursor-pointer"
+								onClick={() => setRate(4)}
+							>
 								⭐⭐⭐⭐
 							</Button>
-							<Button variant="outline" className="cursor-pointer">
+							<Button
+								variant={rate === 5 ? "default" : "outline"}
+								className="cursor-pointer"
+								onClick={() => setRate(5)}
+							>
 								⭐⭐⭐⭐⭐
 							</Button>
 						</section>
@@ -90,10 +267,17 @@ export default function OrderListProduct() {
 								id="note"
 								name="note"
 								placeholder="..."
+								value={comment}
+								onChange={(e) => setComment(e.target.value)}
 								rows={3}
 							></textarea>
 						</section>
-						<Button className="cursor-pointer w-full">Kirim</Button>
+						<Button
+							className="cursor-pointer w-full"
+							onClick={handleOrderSubmit}
+						>
+							Kirim
+						</Button>
 					</section>
 				) : (
 					""
