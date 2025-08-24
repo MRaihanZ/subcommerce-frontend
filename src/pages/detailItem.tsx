fb336b2e61612c2e87a8073827b53d9dcfc684af
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-08-24 18:49:09 +0700

    feat: change sold_products to total_sold_products

--- src/pages/detailItem.tsx
+++ src/pages/detailItem.tsx
@@ -58,7 +58,7 @@ interface SellerSummarize {
 	name: string;
 	img: string;
 	address: string;
-	sold_products: number;
+	total_sold_products: number;
 	average_rating: number;
 }
 
@@ -337,7 +337,9 @@ export default function DetailItem() {
 								>
 									<path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
 								</svg>
-								<p className="ms-2">{seller?.sold_products} Barang Terjual</p>
+								<p className="ms-2">
+									{seller?.total_sold_products} Barang Terjual
+								</p>
 							</section>
 							<p>⭐ {Math.floor(seller?.average_rating * 10) / 10}</p>
 						</section>
