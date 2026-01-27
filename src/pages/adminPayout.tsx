e33cbad43052bf511bc74b7882b1b8d93f6f2fef
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2026-01-27 08:24:02 +0700

    feat: latest update

--- src/pages/adminPayout.tsx
+++ src/pages/adminPayout.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
 
 import { apiUrl } from "@/lib/importEnv";
+
+import { GetCsrf } from "../components/utils/csrf";
 
 import {
 	Table,
@@ -13,50 +14,36 @@ import {
 	TableRow,
 	// TableFooter,
 } from "@/components/ui/table";
-import {
-	Dialog,
-	// DialogClose,
-	DialogContent,
-	DialogDescription,
-	// DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	// DialogTrigger,
-} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 
-import AddAdmin from "@/components/my_components/addAdmin";
-import EditAdmin from "@/components/my_components/editAdmin";
-import DeleteAdmin from "@/components/my_components/deleteAdmin";
-
-interface Admin {
+interface Payout {
 	id: string;
-	name: string;
-	email: string;
+	seller_name: string;
+	created_at: string; // time.Time → ISO string (from JSON)
+
+	transfer_name: string;
+	transfer_type: string;
+	transfer_id: string;
+	transfer_amount: number;
+	transfer_description: string;
+	transfer_status: string | null;
 }
 
 export default function AdminPayout() {
-	const navigate = useNavigate();
-	const [openDialog, setOpenDialog] = useState(false);
-	const [openDialogAction, setOpenDialogAction] = useState<string>("addAdmin");
-
-	const [admins, setAdmins] = useState<Admin[] | null>(null);
-	const [adminSelected, setAdminSelected] = useState<Admin>();
-	const [adminIdSelected, setAdminIdSelected] = useState<string>("");
-	const [search, setSearch] = useState<string>("");
+	const [payouts, setPayouts] = useState<Payout[] | null>(null);
+	// const [search, setSearch] = useState<string>("");
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState<string>();
 
 	const fetchAdmins = async () => {
 		try {
-			const res = await fetch(`${apiUrl}/api/v1/admins/`, {
+			const res = await fetch(`${apiUrl}/api/v1/admins/payout`, {
 				credentials: "include",
 			});
 			const json = await res.json();
 			if (json.code === 200 && json.status === "ok") {
-				setAdmins(json.data);
+				setPayouts(json.data);
 				setLoading(false);
 			} else {
 				toast.error(json.error);
@@ -74,90 +61,83 @@ export default function AdminPayout() {
 		fetchAdmins();
 	}, []);
 
-	const fetchAdmin = async () => {
+	// const fetchAdmin = async () => {
+	// 	try {
+	// 		const res = await fetch(`${apiUrl}/api/v1/admins/` + search, {
+	// 			credentials: "include",
+	// 		});
+	// 		const json = await res.json();
+	// 		if (json.code === 200 && json.status === "ok") {
+	// 			setAdmins(json.data);
+	// 			setLoading(false);
+	// 		} else {
+	// 			toast.error(json.error);
+	// 			setError(json.error);
+	// 			setLoading(false);
+	// 		}
+	// 	} catch (err) {
+	// 		const errFetch = "Network Error: " + err;
+	// 		toast.error(errFetch);
+	// 		setError(errFetch);
+	// 		setLoading(false);
+	// 	}
+	// };
+
+	// const handleSearch = () => {
+	// 	if (search !== "") {
+	// 		fetchAdmin();
+	// 	} else {
+	// 		fetchAdmins();
+	// 	}
+	// };
+
+	// useEffect(() => {
+	// 	if (search === "") {
+	// 		fetchAdmins();
+	// 	}
+	// }, [search]);
+
+	const processPayoutHandler = async (payId: string) => {
+		const csrfToken = await GetCsrf();
+		// const payId = await getPayId();
+
+		const payload = {
+			status: "permintaan sedang diproses",
+		};
+
 		try {
-			const res = await fetch(`${apiUrl}/api/v1/admins/` + search, {
+			const send = await fetch(`${apiUrl}/api/v1/admins/payout/` + payId, {
+				method: "PATCH",
+				headers: {
+					"Content-Type": "application/json",
+					"X-CSRF-TOKEN": csrfToken,
+				},
 				credentials: "include",
+				body: JSON.stringify(payload),
 			});
-			const json = await res.json();
-			if (json.code === 200 && json.status === "ok") {
-				setAdmins(json.data);
-				setLoading(false);
+
+			const result = await send.json();
+			if (result.code === 200 && result.status === "ok") {
+				toast("Berhasil diproses");
+				window.location.reload();
 			} else {
-				toast.error(json.error);
-				setError(json.error);
-				setLoading(false);
+				toast(result.error);
+				setError(result.error);
+				// setLoading(false);
 			}
 		} catch (err) {
 			const errFetch = "Network Error: " + err;
-			toast.error(errFetch);
+			toast(errFetch);
 			setError(errFetch);
-			setLoading(false);
+			// setLoading(false);
 		}
 	};
-
-	const handleSearch = () => {
-		if (search !== "") {
-			fetchAdmin();
-		} else {
-			fetchAdmins();
-		}
-	};
-
-	useEffect(() => {
-		if (search === "") {
-			fetchAdmins();
-		}
-	}, [search]);
-
-	useEffect(() => {
-		if (adminIdSelected !== "") {
-			console.log(adminIdSelected);
-		}
-	}, [adminIdSelected]);
 
 	if (loading) return <p>Loading...</p>;
 	if (error) {
 		toast.error(error);
 	}
-	if (!admins) toast.error("No item found");
-	const childComponentsDialog = (key: string) => {
-		switch (key) {
-			case "addAdmin":
-				return (
-					<>
-						<DialogHeader>
-							<DialogTitle>Tambah Admin</DialogTitle>
-							<DialogDescription></DialogDescription>
-						</DialogHeader>
-						<AddAdmin />
-					</>
-				);
-			case "editAdmin":
-				return (
-					<>
-						<DialogHeader>
-							<DialogTitle>Ubah Admin</DialogTitle>
-							<DialogDescription></DialogDescription>
-						</DialogHeader>
-						<EditAdmin data={adminSelected} />
-					</>
-				);
-			case "deleteAdmin":
-				return (
-					<>
-						<DialogHeader>
-							<DialogTitle>Konfirmasi Hapus Admin</DialogTitle>
-							<DialogDescription></DialogDescription>
-						</DialogHeader>
-						<DeleteAdmin
-							setStateDialog={setOpenDialog}
-							data={adminIdSelected}
-						/>
-					</>
-				);
-		}
-	};
+	if (!payouts) toast.error("No item found");
 	return (
 		<>
 			<section className="my-7">
@@ -196,52 +176,37 @@ export default function AdminPayout() {
 						</TableRow>
 					</TableHeader>
 					<TableBody>
-						{admins.map((admin, index) => (
-							<TableRow key={admin.id}>
-								<TableCell>{admin.id}</TableCell>
+						{payouts?.map((payout) => (
+							<TableRow key={payout.id}>
+								<TableCell>{payout.id}</TableCell>
+								{/* <TableCell className="max-w-50 truncate">
+									{payout.seller_name}
+								</TableCell> */}
+								<TableCell>{payout.seller_name}</TableCell>
+								<TableCell>{payout.transfer_name}</TableCell>
+								<TableCell>{payout.transfer_type}</TableCell>
+								<TableCell>{payout.transfer_id}</TableCell>
+								<TableCell>{payout.transfer_amount}</TableCell>
 								<TableCell className="max-w-50 truncate">
-									{admin.name}
+									{payout.transfer_description}
 								</TableCell>
-								<TableCell className="max-w-50 truncate">
-									{admin.email}
-								</TableCell>
+								<TableCell>{payout.transfer_status}</TableCell>
 								<TableCell>
 									<section className="flex flex-col justify-center items-center gap-3 py-5">
-										<Button
-											variant="outline"
-											className="cursor-pointer w-full"
-											onClick={() => {
-												setAdminSelected({
-													id: admin.id,
-													name: admin.name,
-													email: admin.email,
-												});
-												setOpenDialog(true);
-												setOpenDialogAction("editAdmin");
-											}}
-										>
-											Edit Admin
-										</Button>
-										<Button
-											className="cursor-pointer w-full"
-											variant="destructive"
-											onClick={() => {
-												setAdminIdSelected(admin.id);
-												setOpenDialog(true);
-												setOpenDialogAction("deleteAdmin");
-											}}
-										>
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												height="24px"
-												viewBox="0 -960 960 960"
-												width="24px"
-												fill="currentColor"
+										{payout.transfer_status ===
+										"permintaan sedang dalam antrian" ? (
+											<Button
+												className="cursor-pointer w-full"
+												variant="default"
+												onClick={() => {
+													processPayoutHandler(payout.id);
+												}}
 											>
-												<path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-											</svg>
-											Hapus Admin
-										</Button>
+												Proses
+											</Button>
+										) : (
+											""
+										)}
 									</section>
 								</TableCell>
 							</TableRow>
@@ -256,17 +221,6 @@ export default function AdminPayout() {
 						</TableFooter> */}
 				</Table>
 			</section>
-			<Dialog open={openDialog} onOpenChange={setOpenDialog}>
-				<DialogContent
-					className={
-						openDialogAction !== "deleteAdmin"
-							? "max-h-250 overflow-y-auto"
-							: ""
-					}
-				>
-					{childComponentsDialog(openDialogAction)}
-				</DialogContent>
-			</Dialog>
 		</>
 	);
 }
